7ebbf67293a95c35a58c631435fcfc7ace3aa44c
Torstein S. Ølberg <54394656+MrTorstein@users.noreply.github.com>
2026-06-17 21:02:53 +0200

    Part 3 of assignment 3

---
+++ IIA3220/Assignment_3/Air Heater Visualiser/Air Heater Visualiser/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Air_Heater_Visualiser.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
